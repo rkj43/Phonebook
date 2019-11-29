7ec61a4486628f805db3d2c5ef3082058079edbf
rkj43 <rahulkjohn43@gmail.com>
2019-11-29 12:08:20 +0530

    small tweak

--- Program.cs
+++ Program.cs
@@ -48,21 +48,6 @@ namespace Phonebook
     }
 }
 
- /*         Switch for console and file handling stuff
-            switch (choice)
-                {
-                    case 1: phonebook.AddContact(consoleHandler.GetContacts()); break;
-                    case 2: consoleHandler.ShowContacts(phonebook.Display()); break;
-                    case 3: phonebook.DeleteContact(consoleHandler.GetName()); break;
-                    case 4: consoleHandler.ShowContacts(phonebook.Display(), phonebook.FindContact(consoleHandler.GetName())); break;
-                    case 5: break;
-                    default: Console.WriteLine(" You have entered a wrong option, Please Try again"); break;
-                }
-
-                  FileHandler fileHandler = new FileHandler();
-
-                  phonebook.AddContact(fileHandler.jsonReader());//reading from file for intial.
-                 fileHandler.jsonWriter(phonebook.Display());// storing data into file.
-                */
+ 
 
                 
